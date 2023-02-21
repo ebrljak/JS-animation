48d14efabb07f21acbff1b4f284b2c12606903af
ebrljak <78378657+ebrljak@users.noreply.github.com>
2023-02-21 15:44:59 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const bob = document.querySelector('.face');
+
+let count = 0;
+function moveBob() {
+    count += 50;
+    console.log(count);
+    bob.style.left = count + 'px'
+}
+
+bob.addEventListener('click', moveBob);
